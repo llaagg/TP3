603d189dba2389149e2dcc21dbac24fc80403f34
Pawel Galezowski <pg@revuo.eu>
2026-07-06 10:11:23 +0300

    Refactor: integrate WebDav server functionality; add Server class for handling WebDav requests; update project references in solution and csproj files

---
+++ src/TP3.GUI.WebDav/Server.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Specialized;
+using Common.Logging;
+using WebDAVSharp.Server;
+using WebDAVSharp.Server.Stores.DiskStore;
+
+namespace TP3.GUI.WebDav;
+
+
+public class Server
+{
+    public void Start()
+    {
+        // create properties
+        NameValueCollection properties = new NameValueCollection();
+        properties["showDateTime"] = "true";
+        // set Adapter
+        LogManager.Adapter = new Common.Logging.Simple.ConsoleOutLoggerFactoryAdapter(properties);
+
+
+        WebDavServer server = new WebDavServer(new WebDavDiskStore("C:\\work"));
+        server.Listener.Prefixes.Add("http://localhost/");
+        server.Start();
+    }
+}
