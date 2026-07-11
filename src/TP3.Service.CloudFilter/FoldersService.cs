95ea9309bb17e9dfc52b318b9572fe264befb94c
Pawel Galezowski <pg@revuo.eu>
2026-07-11 10:52:18 +0300

    Refactor: add CloudFilter service and update solution references; include new project in GUI solution

---
+++ src/TP3.Service.CloudFilter/FoldersService.cs
@@ -0,0 +1,27 @@
+﻿using TP3.Interfaces;
+using TP3.Messages;
+
+namespace TP3.Service.CloudFilter;
+
+public class FoldersService : BaseDirectoryNode, IService
+{
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Init(IAgent me)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
