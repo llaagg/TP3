cf4b902d5e72d77b2aa7f01544e890c53b9851e1
Pawel Galezowski <pg@revuo.eu>
2026-07-12 00:29:47 +0300

    Refactor: enhance terminal session handling; implement path extraction and response validation in IPC communication

--- src/TP3.CLI/CLI.ExecuteTerm.cs
+++ src/TP3.CLI/CLI.ExecuteTerm.cs
@@ -26,17 +26,81 @@ public static partial class CLI
 
         // we are connected with tag
         logger.LogInformation("Connected to IPC server with tag {Tag}", tag);
-        var walkRequest = new TP3WalkRequest();
 
+        string currentpath = "/";
         while(true)
         {
-            Console.Write("> ");
+            string newTag = Guid.NewGuid().ToString("N").Substring(0, 8);
+            Console.Write($"{currentpath} ({newTag})> ");
+            // whatwever user will put let's send it to the IPC server
+            // this should be just a path to walk and run Read.
+            
+            // depends whhat we discover afer walk
+            // we either list the folder or we just just run command or with show the file content
+
             var line = Console.ReadLine();
-            if (line == null)
+
+            List<string> path = new List<string>();
+
+            var walkRequest = new TP3WalkRequest();
+            // exctract path from the 
+            if(line != null && line.Trim().Length > 0)
             {
-                break;
+                // split by /
+                path.AddRange(line.Split('/').Where(p => !string.IsNullOrWhiteSpace(p)).Select(p => p.Trim()));
+                walkRequest.Path.Add(path.ToArray());
             }
-            logger.LogInformation("Read line: {Line}", line);
+            
+            walkRequest.NewTag = newTag;
+            var command = new TP3Message()
+            {
+                Tag = tag,
+                WalkRequest = walkRequest
+            }; 
+            // send it if number of returned path is smaller then provided
+            // show a warning 
+            var response = await ipcClient.SendAndWaitOne(command, logger);
+
+            // check error
+            if(response == null)
+            {
+                logger.LogError("No response from IPC server for command: {Command}", line);
+                continue; 
+            }else if(response.PayloadCase == TP3Message.PayloadOneofCase.Error)
+            {
+                logger.LogError("Error from IPC server: {Error}", response.Error?.Message);
+                continue;
+            }
+
+            if(response.PayloadCase == TP3Message.PayloadOneofCase.WalkResponse)
+            {
+                var walkResponse = response.WalkResponse;
+                if(walkResponse == null)
+                {
+                    logger.LogError("Walk response is null for command: {Command}", line);
+                    continue;
+                }
+                
+                // check if path is long as number of nodes returned
+                if(walkResponse.Infos.Count != path.Count)
+                {
+                    logger.LogWarning("Walk response path count {Count} is different from requested path count {RequestedCount} for command: {Command}", walkResponse.Infos.Count, path.Count, line);
+                }
+                
+                var numberOfNodes = walkResponse.Infos.Count;
+
+                currentpath = "/" + string.Join("/", path.Take(numberOfNodes));
+            }
+
+            
+
+            // clunk my friend
+            var clunkRequest = new TP3Message()
+            {
+                Tag = newTag,
+                ClunkRequest = new TP3ClunkRequest()
+            };
+
         }
     }
 }
