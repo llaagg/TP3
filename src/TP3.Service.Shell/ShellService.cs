2caa00060a172d4b47470405f7210bd4b01d4013
Pawel Galezowski <pg@revuo.eu>
2026-07-13 21:06:03 +0300

    Refactor: initialize streams in Shell class and update child node handling

--- src/TP3.Service.Shell/ShellService.cs
+++ src/TP3.Service.Shell/ShellService.cs
@@ -1,4 +1,5 @@
 ﻿using TP3.Interfaces;
+using TP3.Protocol;
 namespace TP3.Service.Shell;
 
 public class ShellService : BaseDirectoryNode, IService
@@ -48,7 +49,6 @@ public class ShellService : BaseDirectoryNode, IService
 
         // we can connect to those streams and use tham as cli terminal
         // we will have some screen buffer and all the nice things
-
         string shortGuid = Guid.NewGuid().ToString().Substring(0, 8);
         var terminalName = $"terminal-{shortGuid}";
         var terminalNode = new Shell(terminalName);
@@ -63,6 +63,23 @@ public class Shell : BaseDirectoryNode
     public Shell(string name) : 
         base(name)
     {
+        this.inStream = new MemoryStream();
+        this.outStream = new MemoryStream();
 
+        // two streams in - and out
+        this.In = new StreamNode(() => inStream);
+        this.Out = new StreamNode(() => outStream);
+
+        this.childs = new List<INode>() { In, Out };
     }
+
+    private Stream inStream;
+    private Stream outStream;
+
+    public StreamNode In { get; }
+    public StreamNode Out { get; }
+
+    private List<INode> childs;
+
+    override public IEnumerable<INode>? Children => this.childs;
 }
