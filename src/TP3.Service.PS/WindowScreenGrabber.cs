8b9e2e21bc13082fefb11ba3d7b6c9cb7e09477a
Pawel Galezowski <pg@revuo.eu>
2026-07-15 15:54:34 +0300

    Update project configuration: specify target framework and supported OS platform, and add System.Drawing.Common package reference

--- src/TP3.Service.PS/WindowScreenGrabber.cs
+++ src/TP3.Service.PS/WindowScreenGrabber.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Diagnostics;
 using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
-
 
 public class WG
 {
