d97d36e78737949b1e972680bc48b0038ec12f37
Pawel Galezowski <pg@revuo.eu>
2026-07-18 21:34:25 +0300

    Refactor act command in build script for improved clarity and functionality

--- build.ps1
+++ build.ps1
@@ -1,6 +1,4 @@
 # you needd: winget install --id Microsoft.PowerShell --source winget
 # and node ()
 
-act -P windows-latest=-self-hosted 
-# --var WINGET_TOKEN=some-value 
-# -s GITHUB_TOKEN="$(gh auth token)" -v
+act -P windows-latest=-self-hosted --var WINGET_TOKEN=some-value -s GITHUB_TOKEN="$(gh auth token)"
