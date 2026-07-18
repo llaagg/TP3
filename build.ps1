0bf942165809dd5ca52de5b2002f054b9bcd03db
Pawel Galezowski <pg@revuo.eu>
2026-07-18 11:28:50 +0300

    Add GitHub token variable to act command in build script for enhanced authentication

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 # you needd: winget install --id Microsoft.PowerShell --source winget
 # and node ()
 
-act -P windows-latest=-self-hosted --var WINGET_TOKEN=some-value
+act -P windows-latest=-self-hosted --var WINGET_TOKEN=some-value -s GITHUB_TOKEN="$(gh auth token)"
