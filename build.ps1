22d2b5bd8079263c28691e82752e00cd7d1a6e67
Pawel Galezowski <pg@revuo.eu>
2026-07-18 21:28:04 +0300

    Add version display step to winget-publish workflow and update build script for clarity

--- build.ps1
+++ build.ps1
@@ -1,4 +1,6 @@
 # you needd: winget install --id Microsoft.PowerShell --source winget
 # and node ()
 
-act -P windows-latest=-self-hosted --var WINGET_TOKEN=some-value -s GITHUB_TOKEN="$(gh auth token)"
+act -P windows-latest=-self-hosted 
+# --var WINGET_TOKEN=some-value 
+# -s GITHUB_TOKEN="$(gh auth token)" -v
