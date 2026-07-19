8b602918104b73ead57ab93593073cfb8fd87431
Pawel Galezowski <pg@revuo.eu>
2026-07-19 23:51:12 +0300

    Refactor: Add GetArguments method to enhance argument parsing in BaseControlParamsArgsCommand

--- src/TP3.Protocol/Base/BaseControlParamsArgsCommand.cs
+++ src/TP3.Protocol/Base/BaseControlParamsArgsCommand.cs
@@ -17,5 +17,28 @@ public abstract class BaseControlParamsArgsCommand : BaseControlCommand
             .Split((char[]?)null, StringSplitOptions.RemoveEmptyEntries);
     }
 
+    private static List<(string argument, string optionalParameter)> GetArguments(params string[]?args)
+    {
+        var result = new List<(string argument, string optionalParameter)>();
+        // find all arguments starting with -- or - and thier respective paramester values
+        
+        for (int i = 0; i < (args?.Length ?? 0); i++)
+        {
+            var arg = args![i];
+            if (arg.StartsWith("--") || arg.StartsWith("-"))
+            {
+                string? optionalParameter = null;
+                if (i + 1 < args.Length && !args[i + 1].StartsWith("--") && !args[i + 1].StartsWith("-"))
+                {
+                    optionalParameter = args[i + 1];
+                    i++;
+                }
+                result.Add((arg, optionalParameter));
+            }
+        }
+
+        return result;
+    }
+
     protected abstract Task HandleParamsArgsCommand(Stream output, params string[]? args);
 }
