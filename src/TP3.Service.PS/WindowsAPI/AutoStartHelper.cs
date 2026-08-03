874202282a257b1b88479028ac59a8dcb394da32
Pawel Galezowski <pg@revuo.eu>
2026-08-03 10:33:33 +0300

    Refactor: Update AutoStartAppEnabled to use RWStreamString for improved data handling and introduce RWStream for enhanced stream functionality

--- src/TP3.Service.PS/WindowsAPI/AutoStartHelper.cs
+++ src/TP3.Service.PS/WindowsAPI/AutoStartHelper.cs
@@ -65,7 +65,82 @@ public class AutoStartAppEnabled : INode
 
     public async Task<ITP3DataStream?> Get()
     {
-        var enabledValue = parent.Enabled ? "true" : "false";
-        return await Task.FromResult<ITP3DataStream?>(new TP3Stream(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(enabledValue))));
+        return new RWStreamString(
+            readFunc: async () =>
+            {
+                return parent.Enabled ? "1" : "0";
+            },
+            writeFunc: async (value) =>
+            {
+               throw new NotImplementedException("Writing to the enabled property is not implemented."); 
+            });
+    }
+}
+
+public abstract class RWStream : ITP3DataStream
+{
+    public uint Iounit => 0;
+
+    public ulong Position { get; protected set; } = 0;
+
+    public void Close()
+    {
+    }
+
+    public async Task Open()
+    {
+        
+    }
+
+    public async Task<byte[]> Read(ulong offset, ulong maxCount)
+    {
+        return await OnRead(offset, maxCount);
+    }
+
+    public async Task<ulong> Write(ulong offset, byte[] data)
+    {
+        var incomingData = data;
+        await OnWrite(incomingData);
+        return (ulong)data.Length;
+    }
+
+    public virtual async Task OnWrite(byte[] data)
+    {
+        
+    }
+
+    public virtual async Task<byte[]> OnRead(ulong offset, ulong maxCount)
+    {
+        return new byte[0];
+    }
+}
+
+public class RWStreamString : RWStream
+{
+    public RWStreamString(Func<Task<string>> readFunc, Func<string, Task> writeFunc)
+    {
+        this.readFunc = readFunc;
+        this.writeFunc = writeFunc;
+    }
+
+    private Func<Task<string>> readFunc;
+    private Func<string, Task> writeFunc;
+
+    public override async Task OnWrite(byte[] data)
+    {
+        var incomingData = System.Text.Encoding.UTF8.GetString(data);
+        await writeFunc(incomingData);
+    }
+
+    public override async Task<byte[]> OnRead(ulong offset, ulong maxCount)
+    {
+        if(offset != 0)
+        {
+            return new byte[0];
+        }
+
+        var result = await readFunc();
+        this.Position = (ulong)result.Length;
+        return System.Text.Encoding.UTF8.GetBytes(result);
     }
 }
